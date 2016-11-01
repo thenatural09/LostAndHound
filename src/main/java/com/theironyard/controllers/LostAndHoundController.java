8506c947f211deab40236fcb0b4081fd686b2145
Troy <tward4@tulane.edu>
2016-10-31 23:28:27 -0400

    Adding html and password storage and starting work on controller

--- src/main/java/com/theironyard/controllers/LostAndHoundController.java
+++ src/main/java/com/theironyard/controllers/LostAndHoundController.java
@@ -1,7 +1,18 @@
 package com.theironyard.controllers;
 
+import com.theironyard.services.DogRepository;
+import com.theironyard.services.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Troy on 10/31/16.
  */
+@Controller
 public class LostAndHoundController {
+    @Autowired
+    DogRepository dogs;
+
+    @Autowired
+    UserRepository users;
 }
